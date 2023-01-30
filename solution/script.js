86772d6d0c6481af78c1f72a7b1275a530772a41
TaylorDarneille <tmdarneille@gmail.com>
2023-01-30 03:16:49 -0800

    instructions complete

--- solution/script.js
+++ solution/script.js
@@ -1,8 +1,8 @@
 const d = new Date()
 
+const monthIndex = d.getMonth()
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-
-const month = months[d.getMonth()]
+const month = months[monthIndex]
 
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 const day = days[d.getDay()]
